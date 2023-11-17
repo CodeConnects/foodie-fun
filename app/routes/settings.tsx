ff3a3f8b080c725363350e7130dbc0cb29732c8f
CodeConnects <codeconnects@gmail.com>
2023-11-17 12:49:32 -0500

    add default settings route

--- app/routes/settings.tsx
+++ app/routes/settings.tsx
@@ -1,7 +1,15 @@
+import { Link } from "remix";
+
 export default function Settings() {
   return (
     <div>
       <h1>Settings</h1>
+
+      <nav>
+        <Link to="app">App</Link>
+        <Link to="profile">Profile</Link>
+      </nav>
+
     </div>
   );
 }

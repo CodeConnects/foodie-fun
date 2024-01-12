f03bfccc7ace915fcb6dcef26de8ce78900bd3fb
CodeConnects <codeconnects@gmail.com>
2024-01-12 10:39:59 -0400

    add main navigation and main global tailwind file

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,5 @@
 import { cssBundleHref } from "@remix-run/css-bundle";
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction, LinksFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -8,7 +8,13 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import { Link } from "react-router-dom";
 
+// add main tailwind css file
+import styles from "./tailwind.css";
+export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
+
+// meta description for all pages
 export const meta: MetaFunction = () => {
   return [
     { title: "Remix Foodie Fun" },
@@ -26,6 +32,31 @@ export default function App() {
         <Links />
       </head>
       <body>
+
+        <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+          <Link to="/" className="flex items-center flex-shrink-0 text-white mr-6">Foodie Fun</Link>
+          <div className="block lg:hidden">
+            <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+              <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <title>Menu</title>
+                <path d="M0 0h20v20H0z" fill="none"/>
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"
+                />
+              </svg>
+            </button>
+          </div>
+          <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+            <div className="text-sm lg:flex-grow">
+              <Link to="discover" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">Discover</Link>
+              <Link to="app" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">App</Link>
+              <Link to="settings" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">Settings</Link>
+            </div>
+          </div>
+        </nav>
+
         <Outlet />
         <ScrollRestoration />
         <Scripts />

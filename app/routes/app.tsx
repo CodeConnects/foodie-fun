f38786c54f62ff87e08ac1f0cf22a5e52754d0fe
CodeConnects <codeconnects@gmail.com>
2024-01-15 18:14:53 -0500

    add filler app text

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -2,6 +2,11 @@ export default function App() {
   return (
     <div>
       <h1>App</h1>
+      <ul className="m-3">
+        <li>App</li>
+        <li>App</li>
+        <li>App</li>
+      </ul>
     </div>
   );
 }

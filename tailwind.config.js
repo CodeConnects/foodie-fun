55f5728f30e397efbd10ed65985d5d16937c1f12
CodeConnects <codeconnects@gmail.com>
2024-01-14 22:39:27 -0500

    change to blue color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,16 @@ export default {
   theme: {
     extend: {
       colors: {
-        'primary': '#2d545e',/*Night Blue: #2d545e*/
-        'secondary': '#c89666',/*Sand Tan Shadow: #c89666*/
-        'tertiary': '#C5C6C7',/*Sand Tan: #e1b382*/
-        'quaternary': '#12343b',/*Night Blue Shadow: #12343b*/
+        //'primary': '#2d545e',/*Night Blue: #2d545e*/
+        'primary': '#2a6592',
+        //'secondary': '#c89666',/*Sand Tan Shadow: #c89666*/
+        'secondary': '#8ec3eb',
+        //'tertiary': '#C5C6C7',/*Sand Tan: #e1b382*/
+        'tertiary': '#7e9a9a',
+        //'quaternary': '#12343b',/*Night Blue Shadow: #12343b*/
+        'quaternary': '#f6d8ac',
       },
     },
   },
   plugins: [],
 }
-

6483ee9fc033c1ababfebc2eb3385334a3a9810b
CodeConnects <codeconnects@gmail.com>
2024-01-15 18:14:35 -0500

    add short description to home page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,6 +2,7 @@ export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>Recipe Remix</h1>
+      <p className="m-3">So many tasty recipes to share</p>
     </div>
   );
 }

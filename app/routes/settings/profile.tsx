067671c49bf5233a41cef9e0112e29c02278c557
CodeConnects <codeconnects@gmail.com>
2024-01-15 17:56:47 -0500

    add name, email and password settings holders

--- app/routes/settings/profile.tsx
+++ app/routes/settings/profile.tsx
@@ -2,6 +2,11 @@ export default function Profile() {
   return (
     <div>
       <h1>Profile Settings</h1>
+      <ul>
+        <li>Name</li>
+        <li>Email</li>
+        <li>Password</li>
+      </ul>
     </div>
   );
 }

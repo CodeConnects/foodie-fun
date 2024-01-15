164497e3730957b2b3f60539ffefc0685082c426
CodeConnects <codeconnects@gmail.com>
2024-01-15 18:14:45 -0500

    add filler discover text

--- app/routes/discover.tsx
+++ app/routes/discover.tsx
@@ -2,6 +2,11 @@ export default function Discover() {
   return (
     <div>
       <h1>Discover</h1>
+      <ul className="m-3">
+        <li>Discover</li>
+        <li>Discover</li>
+        <li>Discover</li>
+      </ul>
     </div>
   );
 }

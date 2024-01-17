6e3353c4ab21fc81e3149efd232e097877ec98c0
CodeConnects <codeconnects@gmail.com>
2024-01-17 17:24:29 -0500

    add Weather API with 7 days of Miami instead of color shapes

--- app/routes/apiFetcher.tsx
+++ app/routes/apiFetcher.tsx
@@ -2,32 +2,41 @@ import { LoaderFunction } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 
 export const loader: LoaderFunction = async () => {
-  const response = await fetch("https://jsonplaceholder.typicode.com/photos");
-  const photos = await response.json();
-  //console.log(photos[5]);
 
-  /*const data = { message: "Hello from the API!" };
-
-  const reply = new Response(JSON.stringify(data), {
-    status: 269,
-    headers: { "content-type": "application/json" },
-  });
-
-  return reply;*/
-  return photos;
+  const key = 'ae7a5ef0d7bc4855965204819241701';
+  const q = 'miami';
+  const days = '7';
+  const aqi = 'no';
+  const alerts = 'no';
+  const url = 'http://api.weatherapi.com/v1/forecast.json?key='+key+'&q='+q+'&days='+days+'&aqi='+aqi+'&alerts='+alerts;
+  
+  const weather = await fetch(url);
+  const weatherData = await weather.json();
+  //console.log("weatherData: "+weatherData);
+  return weatherData;
 }
 
 export default function ApiFetcher() {
   const data: any = useLoaderData();
   return (
     <div>
-      <h1>API Fetcher</h1>
-      <p>Fetches data from an API and displays it.</p>
-      <p>Here's the data:</p>
+      <h1>WeatherAPI</h1>
+      <h2>&nbsp;</h2>
+      <img style={{float: 'left'}} src={`${data.current.condition.icon}`} />
+      <h2 style={{paddingLeft: '70px', paddingTop: '6px', width: '125%', lineHeight: '32px'}}>On {data.location.localtime} it is <span style={{color: 'yellow'}}>{data.current.temp_f}° F and {data.current.condition.text}</span></h2>
+      <h2 style={{paddingLeft: '70px', width: '125%', lineHeight: '32px'}}>In <span style={{color: 'yellow'}}>{data.location.name} {data.location.region}</span>, lat {data.location.lat} long {data.location.lon}</h2>
 
-      <h3><img src={data[12].thumbnailUrl} alt={data[5].title} />{data[5].title}</h3>
-      <p><img src={data[10].url} alt={data[5].title} /></p>
-      
+      <ul>
+        {data.forecast.forecastday.map((day: any) => (
+          <li key={day.date} style={{textAlign: 'center', float: 'left', margin: '50px 50px 10px 0px'}}>
+            <p>{day.day.condition.text}</p>
+            <img style={{marginLeft: 'auto', marginRight: 'auto'}} src={`${day.day.condition.icon}`} />
+            <p>High: {day.day.maxtemp_f}° F</p>
+            <p>Low: {day.day.mintemp_f}° F</p>
+            <p>{day.date}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }

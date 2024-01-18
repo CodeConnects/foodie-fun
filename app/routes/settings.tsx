3004b4051d0856e442c1feee25933440f815acd4
CodeConnects <codeconnects@gmail.com>
2024-01-18 13:59:15 -0500

    add a loader message

--- app/routes/settings.tsx
+++ app/routes/settings.tsx
@@ -1,9 +1,17 @@
-import { NavLink, Outlet } from "@remix-run/react";
+import { NavLink, Outlet, useLoaderData } from "@remix-run/react";
+import { LoaderFunction, json } from "@remix-run/node";
+
+export const loader: LoaderFunction = () => {
+  return json({ message: "_________________" });
+}
 
 export default function Settings() {
+  const data = useLoaderData<typeof loader>();
+
   return (
     <div>
       <h1>Settings</h1>
+      <p>{data.message}</p>
 
       <nav className="page-nav my-5">
         <NavLink
